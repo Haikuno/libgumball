4d240ab04e477a7c08bee919c8c742152a76e2ad
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-22 00:26:34 -0300

    make mouse events carry pointer snapshots

--- lib/api/gumball/events/gumball_event_mouse.h
+++ lib/api/gumball/events/gumball_event_mouse.h
@@ -7,16 +7,15 @@
  *   \ref     GUM_Event_Mouse "GUM_Event_Mouse data structure and hierarchy graph"
  *   \ingroup events
  *
- *   Brief description
- *
- *   Extended
- *   Description.
+ *   Mouse button event. In addition to the common input state/action fields,
+ *   it snapshots pointer position, movement delta, and wheel delta from the
+ *   emitting mouse device.
  *
  *   \author    2026 Agustín Bellagamba
  *   \copyright MIT License
 */
 
-#include <gumball/events/gumball_event_input.h>
+#include <gumball/events/gumball_event_pointer.h>
 
 /*! \name  Type System
  *  \brief Type UUID and cast operators
@@ -32,34 +31,21 @@
 
 GBL_DECLS_BEGIN
 GBL_FORWARD_DECLARE_STRUCT(GUM_Event_Mouse);
+GBL_FORWARD_DECLARE_STRUCT(GUM_Mouse);
 
-/*!
- *    \struct  GUM_Event_MouseClass
- *    \extends GUM_Event_InputClass
- *    \brief   GUM_Event_Mouse structure
- *
- *    GUM_Event_MouseClass derives from GUM_Event_InputClass,
- *    TODO: description
-*/
-//! \cond
-GBL_CLASS_DERIVE(GUM_Event_Mouse, GUM_Event_Input)
-    //
-GBL_CLASS_END
-//! \endcond
+GBL_CLASS_DERIVE_EMPTY(GUM_Event_Mouse, GUM_Event_Pointer)
 
-/*!
- *    \class   GUM_Event_Mouse
- *    \extends GUM_Event_Input
- *    \brief   TODO: brief description
-*/
-GBL_INSTANCE_DERIVE(GUM_Event_Mouse, GUM_Event_Input)
-    //
+GBL_INSTANCE_DERIVE(GUM_Event_Mouse, GUM_Event_Pointer)
+    GUM_Vector2 delta; //!< Mouse movement delta when the event was created.
+    GUM_Vector2 wheel; //!< Wheel delta when the event was created.
 GBL_INSTANCE_END
 
 GblType GUM_Event_Mouse_type(void) GBL_NOEXCEPT;
 
-//! Returns a new GUM_Event_Mouse.
+//! Returns a new zero-initialized GUM_Event_Mouse.
 #define GUM_Event_Mouse_create() GUM_EVENT_MOUSE(GblEvent_create(GUM_EVENT_MOUSE_TYPE))
+//! Creates a mouse event and snapshots position/delta/wheel/device from pMouse.
+GUM_Event_Mouse* GUM_Event_Mouse_createFrom(const GUM_Mouse* pMouse) GBL_NOEXCEPT;
 
 GBL_DECLS_END
 #undef GBL_SELF_TYPE
