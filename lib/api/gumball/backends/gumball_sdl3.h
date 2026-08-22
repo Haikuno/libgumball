ba1cd52bd8b9b18b1a1f91dd61191530a5c244f4
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-21 23:54:36 -0300

    document SDL renderer destruction order

--- lib/api/gumball/backends/gumball_sdl3.h
+++ lib/api/gumball/backends/gumball_sdl3.h
@@ -5,7 +5,7 @@
  *   \brief SDL3 backend integration
  *
  *   \author     2026 Agustín Bellagamba
- *   \copyright  MIT License
+ *   \copyright MIT License
 */
 
 #include <SDL3/SDL.h>
@@ -13,7 +13,12 @@
 
 GBL_DECLS_BEGIN
 
-//! Creates a libGumball renderer wrapper around an SDL_Renderer.
+/*! \brief Creates a libGumball renderer wrapper around an application-owned SDL_Renderer.
+ *
+ *  The SDL_Renderer remains owned by the caller. Destroy the returned libGumball wrapper
+ *  with GUM_Renderer_destroy() before calling SDL_DestroyRenderer() so libGumball can
+ *  invalidate renderer-specific cached resources while the SDL renderer is still valid.
+*/
 GUM_Renderer* GUM_SDL3_Renderer_create(SDL_Renderer* pRenderer);
 //! Feeds an SDL event to libGumball without taking ownership of the application's event loop.
 void          GUM_SDL3_processEvent(const SDL_Event* pEvent);
