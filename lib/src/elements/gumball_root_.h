8b730bae113b56a46f528ab9e6128fafdd0685c6
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-22 02:53:28 -0300

    add private Root runtime contract

---
+++ lib/src/elements/gumball_root_.h
@@ -0,0 +1,17 @@
+#ifndef GUMBALL_ROOT_PRIVATE_H
+#define GUMBALL_ROOT_PRIVATE_H
+
+#include <gumball/elements/gumball_root.h>
+#include <gumball/types/gumball_vector2.h>
+
+GBL_FORWARD_DECLARE_STRUCT(GUM_Widget);
+GBL_FORWARD_DECLARE_STRUCT(GUM_Renderer);
+
+GUM_Root*   GUM_Root_active_           (void);
+GBL_RESULT  GUM_Root_drawEnable_       (GUM_Widget* pWidget);
+void        GUM_Root_drawDisable_      (GUM_Widget* pWidget);
+void        GUM_Root_drawOrderChanged_ (GUM_Widget* pWidget);
+GBL_RESULT  GUM_Root_draw_             (GUM_Root* pRoot, GUM_Renderer* pRenderer);
+GUM_Widget* GUM_Root_pointerTargetAt_  (GUM_Root* pRoot, GUM_Vector2 position);
+
+#endif // GUMBALL_ROOT_PRIVATE_H
