aea8e3fb500e581620dc561caf5d2520b8b2e60a
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-21 22:46:50 -0300

    fix: deinit raylib backend resources

--- lib/src/backends/raylib/gumball_raylib_time.c
+++ lib/src/backends/raylib/gumball_raylib_time.c
@@ -1,8 +1,13 @@
 #include <gumball/core/gumball_backend.h>
 #include <raylib.h>
 
+void GUM_Raylib_Font_deinit(void);
+
 void GUM_Backend_update(void) {}
-void GUM_Backend_deinit(void) {}
+
+void GUM_Backend_deinit(void) {
+    GUM_Raylib_Font_deinit();
+}
 
 uint32_t GUM_Backend_timestamp(void) {
     return (uint32_t)(GetTime() * 1000);
