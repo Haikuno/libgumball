7c168b571d293d6e6d6d586240802188543b1c76
Agustín Bellagamba <67402084+Haikuno@users.noreply.github.com>
2026-08-22 01:24:01 -0300

    add generic pointer input device

---
+++ lib/api/gumball/devices/gumball_pointer.h
@@ -0,0 +1,52 @@
+#ifndef GUM_POINTER_H
+#define GUM_POINTER_H
+
+// View this file's documentation online: TODO: add link
+
+/*!  \file
+ *   \ref     GUM_Pointer "GUM_Pointer data structure and hierarchy graph"
+ *   \ingroup devices
+ *
+ *   Base input device for a single positional pointing source. It stores
+ *   absolute position and relative motion while leaving device-specific state
+ *   such as a mouse wheel to derived device types.
+ *
+ *   \author    2026 Agustín Bellagamba
+ *   \copyright MIT License
+*/
+
+#include "gumball_inputdevice.h"
+#include <gumball/types/gumball_vector2.h>
+
+/*! \name  Type System
+ *  \brief Type UUID and cast operators
+ *  @{
+*/
+#define GUM_POINTER_TYPE            (GBL_TYPEID(GUM_Pointer))
+#define GUM_POINTER(self)           (GBL_CAST(GUM_Pointer, self))
+#define GUM_POINTER_CLASS(klass)    (GBL_CLASS_CAST(GUM_Pointer, klass))
+#define GUM_POINTER_CLASSOF(self)   (GBL_CLASSOF(GUM_Pointer, self))
+//! @}
+
+#define GBL_SELF_TYPE GUM_Pointer
+
+GBL_DECLS_BEGIN
+GBL_FORWARD_DECLARE_STRUCT(GUM_Pointer);
+
+GBL_CLASS_DERIVE_EMPTY(GUM_Pointer, GUM_InputDevice)
+
+GBL_INSTANCE_DERIVE(GUM_Pointer, GUM_InputDevice)
+    GUM_Vector2 position; //!< Current pointer position.
+    GUM_Vector2 delta;    //!< Motion delta from the previous update.
+GBL_INSTANCE_END
+
+GblType GUM_Pointer_type(void) GBL_NOEXCEPT;
+
+//! Returns a new GUM_Pointer. Optionally takes Name/Value property pairs.
+#define GUM_Pointer_create(/* property_name, property_value */...) GBL_NEW(GUM_Pointer __VA_OPT__(,) __VA_ARGS__)
+
+GBL_DECLS_END
+
+#undef GBL_SELF_TYPE
+
+#endif // GUM_POINTER_H
